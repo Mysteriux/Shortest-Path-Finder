ab234a0ab9fba03a38103482e3546313ab8ea4aa
Mysteriux <dipku2244@gmail.com>
2025-04-24 02:41:41 +0530

    backend fixed for deployment

--- server.py
+++ server.py
@@ -129,4 +129,6 @@ def shortestPath(a, b):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT variable
+    app.run(host='0.0.0.0', port=port)
